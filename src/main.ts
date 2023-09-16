25b31870c99787d8faae92cf856582585aa21201
Evgeniy Shabolin <steamguardemail@yandex.ru>
2023-09-16 19:56:22 +0300

    upd files

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { NestExpressApplication } from "@nestjs/platform-express";
 import { join } from "path"
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
 
   app.enableCors()
   app.useStaticAssets(join(__dirname, '..', 'static'))
